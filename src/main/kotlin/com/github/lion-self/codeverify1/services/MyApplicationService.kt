61b272ba0d4f4a4c6296872bbba053028a867466
GitHub Action <action@github.com>
2020-10-06 08:35:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/lion-self/codeverify1/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lion-self/codeverify1/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lion-self.codeverify1.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lion-self.codeverify1.MyBundle
 
 class MyApplicationService {
 
